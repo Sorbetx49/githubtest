34760d8fd3baf9e8faa6a92980a82a0d46cab6a0
Sorbetx49 <fabnwei49@gmail.com>
2020-08-06 15:47:05 +0800

    add atb rank

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ public class Game {
 	private int rolls[] = new int[21];
   private int currentRoll = 0;
   private int record = 0;
+  private String rank = null;
 
 	public void roll(int pins) {
 		rolls[currentRoll++] = pins;
